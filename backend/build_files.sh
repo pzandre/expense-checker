6c9e55536af66e75c0da89ffe14a06f47dad8880
Andre Perez <andremztm@gmail.com>
2026-02-04 13:06:05 -0300

    chore: use uv

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,3 @@
-echo "BUILD START"
-python3.13 -m pip install -r requirements.txt
+#!/bin/bash
+uv pip install -r requirements.txt
 python manage.py migrate
-# echo "$POSTGRES_SSLROOTCERT" | base64 --decode > cert.crt
-echo "BUILD END"
