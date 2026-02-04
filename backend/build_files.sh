6271de213c92b6d25f6c80553fb1bf3f6944bb03
Andre Perez <andremztm@gmail.com>
2026-02-04 13:05:16 -0300

    chore: adjust script

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
+echo "BUILD START"
+python3.13 -m pip install -r requirements.txt
 python manage.py migrate
+# echo "$POSTGRES_SSLROOTCERT" | base64 --decode > cert.crt
+echo "BUILD END"
